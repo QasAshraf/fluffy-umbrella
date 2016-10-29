feaf182399865e23387a24d0146d2747e4d1bf08
Shane Egan <egnshane@gmail.com>
2016-10-29 18:19:57 +0100

    Server - controller comms ping pong

--- server.js
+++ server.js
@@ -1,9 +1,30 @@
 var express = require('express');
 var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
-app.use(express.static('static'));
 app.use(express.static('bower_components/phaser/build'));
+app.use(express.static('node_modules/socket.io-client'));
 
-app.listen(8080, function () {
+app.use(express.static('client'));
+
+app.use('/controller', express.static('controller'));
+
+
+http.listen(8080, function () {
     console.log('Game server listening on port 8080!');
+});
+
+io.on('connection', function(socket){
+    console.log('a user connected');
+
+    socket.on('disconnect', function(){
+        console.log('user disconnected');
+    });
+
+    socket.on('controllerData', function(data){
+        console.log('controllerData: ' + data);
+
+        this.emit('serverBroadcast', "pong");
+    });
 });
